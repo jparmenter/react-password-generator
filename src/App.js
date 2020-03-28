45eb649b31e1a41cdddc2c2b5443ac89c2633604
Jeremy Parmenter <shabado9889@gmail.com>
2020-03-28 16:52:57 -0700

    Add initial generatored code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Container from "@material-ui/core/Container";
+import Grid from "@material-ui/core/Grid";
+import Typography from "@material-ui/core/Typography";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container maxWidth="md" fixed>
+      <Grid
+        container
+        direction="row"
+        justify="center"
+        alignItems="flex-start"
+        style={{ backgroundColor: "#cfe8fc", height: "50vh" }}
+      >
+        <Typography variant="h1" component="h1" style={{ fontSize: '5rem' }}>
+          Password Generator
+        </Typography>
+      </Grid>
+    </Container>
   );
 }
 
