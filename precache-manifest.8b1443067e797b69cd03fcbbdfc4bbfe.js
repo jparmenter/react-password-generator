babf14ae926ca2b511cb7a57a23c77adbed533e5
Jeremy Parmenter <shabado9889@gmail.com>
2020-04-16 15:11:35 -0700

    Updates

---
+++ precache-manifest.8b1443067e797b69cd03fcbbdfc4bbfe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6e45f2fa78a4d0a0b04d30583554d0e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "020d8450f8b272cdb26d",
+    "url": "/static/css/main.ce5da68f.chunk.css"
+  },
+  {
+    "revision": "81b6b9f411d1692cc7e4",
+    "url": "/static/js/2.e05003dc.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.e05003dc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "020d8450f8b272cdb26d",
+    "url": "/static/js/main.0da495d6.chunk.js"
+  },
+  {
+    "revision": "1e353c4edc977a4e72f4",
+    "url": "/static/js/runtime-main.f06a0401.js"
+  }
+]);
